31763b265fabc4f16905b2f864d74c04a5205a0d
leedonghwi <meu12002@gmail.com>
2020-09-05 01:32:12 +0900

    Refactor sources

--- app/src/main/java/com/meuus90/daumbooksearch/model/data/source/repository/book/BooksRepository.kt
+++ app/src/main/java/com/meuus90/daumbooksearch/model/data/source/repository/book/BooksRepository.kt
@@ -2,11 +2,15 @@ package com.meuus90.daumbooksearch.model.data.source.repository.book
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.meuus90.base.constant.AppConfig
 import com.meuus90.daumbooksearch.model.data.source.api.DaumAPI
 import com.meuus90.daumbooksearch.model.data.source.local.Cache
+import com.meuus90.daumbooksearch.model.paging.PagingDataInterface
 import com.meuus90.daumbooksearch.model.paging.book.BooksPageKeyedMediator
+import com.meuus90.daumbooksearch.model.schema.book.BookDoc
 import com.meuus90.daumbooksearch.model.schema.book.BookRequest
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -14,7 +18,8 @@ import javax.inject.Singleton
 @Singleton
 class BooksRepository
 @Inject
-constructor(private val db: Cache, private val daumAPI: DaumAPI) : BookInterface {
+constructor(private val db: Cache, private val daumAPI: DaumAPI) :
+    PagingDataInterface<BookRequest, Flow<PagingData<BookDoc>>> {
 
     override fun execute(bookSchema: BookRequest) = Pager(
         config = PagingConfig(
